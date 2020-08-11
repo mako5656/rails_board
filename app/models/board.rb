25ce68b31685672b12d8ba2a002b6a6283547308
makoto <makoto09142425@gmail.com>
2020-08-11 11:38:06 +0900

    多対多のアソシエーションの設定

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,7 @@
 class Board < ApplicationRecord
     has_many :comments
+    has_many :board_tag_relations
+    has_many :tags, through: :board_tag_relations
 
     validates :name, presence: true, length: { maximum: 10 }
     validates :title, presence: true, length: { maximum: 30 }
