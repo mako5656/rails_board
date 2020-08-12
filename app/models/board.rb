493d9264c3c02e4c02251cd9f0f2744306e5a27d
makoto <makoto09142425@gmail.com>
2020-08-13 08:44:52 +0900

    掲示板へのタグ付け機能追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,5 @@
 class Board < ApplicationRecord
-    has_many :comments: dependent: :delete_all
+    has_many :comments, dependent: :delete_all
     has_many :board_tag_relations, dependent: :delete_all
     has_many :tags, through: :board_tag_relations
 
